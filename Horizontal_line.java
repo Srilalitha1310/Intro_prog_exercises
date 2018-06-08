85f9af53e7738bf008f080c583299ae82dfdf5cb
Srilalitha1310 <40050854+Srilalitha1310@users.noreply.github.com>
2018-06-08 14:07:23 +0530

    Create Horizontal_line.java

---
+++ Horizontal_line.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n, i;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (i = 1; i <= n; i++)
+            System.out.print("*");
+    }
+}
