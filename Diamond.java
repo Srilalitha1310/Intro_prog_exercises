d702e54fe0c267e3d87b476aced27ef774e96889
Srilalitha1310 <40050854+Srilalitha1310@users.noreply.github.com>
2018-06-08 14:29:04 +0530

    Create Diamond.java

---
+++ Diamond.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n,k, i,j,count=1;
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int space=n-1;
+        for (i = 1; i <= n; i++)
+        {
+            for(k=1;k<=space;k++)
+            {
+                System.out.print(" ");
+            }
+            space--;
+            for(j=1;j<=count;j++)
+            {
+                System.out.print("*");
+            }
+            count+=2;
+            System.out.println();
+        }
+        space=1;
+        count-=4;
+        for(i=n-1;i>=1;i--)
+        {
+            for(k=1;k<=space;k++)
+            {
+                System.out.print(" ");
+            }
+            space++;
+            for(j=1;j<=count;j++)
+            {
+                System.out.print("*");
+            }
+            count-=2;
+            System.out.println();
+        }
+    }
+}
